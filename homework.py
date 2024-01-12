5453347322443da640d9e90454165637dd91dd50
Максим Лесков <max.leskoff@icloud.com>
2024-01-12 16:28:19 +0300

    second commit, microfix

--- homework.py
+++ homework.py
@@ -16,28 +16,30 @@ logger = logging.getLogger(__name__)
 handlers = logging.StreamHandler(sys.stdout)
 
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 
-class RequestStatusNotOk(Exception):
-    """Exception for status request != 200"""
+class RequestStatusNotOkError(Exception):
+    """Exception for status request != 200."""
+
     pass
 
 
-class EmptyAnswerAPI(Exception):
-    """Empty answer in api request"""
+class EmptyAnswerAPIError(Exception):
+    """Empty answer in api request."""
+
     pass
 
 
@@ -50,7 +52,7 @@ def handle_exception(exceptions: tuple[type[Exception]] = (Exception,)):
             try:
                 func(*args, **kwargs)
             except exceptions as error:
-                logging.error(f'Произошла ошибка: {error}')
+                logging.error(f"Произошла ошибка: {error}")
 
         return inner_decorator
 
@@ -62,64 +64,68 @@ def check_tokens():
     variables = {
         PRACTICUM_TOKEN: "PRACTICUM_TOKEN",
         TELEGRAM_TOKEN: "TELEGRAM_TOKEN",
-        TELEGRAM_CHAT_ID: "TELEGRAM_CHAT_ID"
+        TELEGRAM_CHAT_ID: "TELEGRAM_CHAT_ID",
     }
     for variable in variables:
         if not variable:
             logging.critical(
-                f'Отсутствует переменная среды {variables.get(variable)}',
+                f"Отсутствует переменная среды {variables.get(variable)}",
                 exc_info=True
             )
-            sys.exit(f'Отсутствует переменная среды {variables.get(variable)},'
-                     f' невозможно продолжить работу')
+            sys.exit(
+                f"Отсутствует переменная среды {variables.get(variable)},"
+                f" невозможно продолжить работу"
+            )
 
 
 @handle_exception(exceptions=(telegram.TelegramError,))
 def send_message(bot, message):
     """Send message."""
-    logging.info(f'Начало отправки сообщений в Telegram')
+    logging.info("Начало отправки сообщений в Telegram")
     bot.send_message(
         chat_id=TELEGRAM_CHAT_ID,
         text=message,
     )
 
-    logging.debug(f'Отправлено сообщение: {message}')
+    logging.debug(f"Отправлено сообщение: {message}")
 
 
 def get_api_answer(timestamp):
     """Request endpoint."""
     try:
-        params = {'from_date': timestamp}
-        logging.info(f'Отправка запроса на {ENDPOINT}, параметры: {params}')
+        params = {"from_date": timestamp}
+        logging.info(f"Отправка запроса на {ENDPOINT}, параметры: {params}")
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         if response.status_code != HTTPStatus.OK:
-            return RequestStatusNotOk(f'Эндпоинт {response.url} получил статус отличный от 200.')
+            return RequestStatusNotOkError(
+                f"Эндпоинт {response.url} получил статус отличный от 200."
+            )
     except requests.RequestException as error:
-        return Exception(f'Возникла ошибка: {error}')
+        return Exception(f"Возникла ошибка: {error}")
     return response.json()
 
 
 def check_response(response):
     """Check response for correct."""
     if not isinstance(response, dict):
-        raise TypeError('Получен не верный тип данных')
-    homeworks = response.get('homeworks')
+        raise TypeError("Получен не верный тип данных")
+    homeworks = response.get("homeworks")
     if homeworks is None:
-        raise Exception('Пустой ответ после запроса к АПИ')
+        raise RequestStatusNotOkError("Пустой ответ после запроса к АПИ")
     if not isinstance(homeworks, list):
-        raise TypeError('Домашки пришли не списком')
+        raise TypeError("Домашки пришли не списком")
     return homeworks
 
 
 def parse_status(homework: dict):
     """Parse status in response."""
-    homework_name = homework.get('homework_name')
+    homework_name = homework.get("homework_name")
     if not homework_name:
-        raise Exception('Отсутствует название домашней работы')
-    homework_status = homework.get('status')
+        raise Exception("Отсутствует название домашней работы")
+    homework_status = homework.get("status")
     verdict = HOMEWORK_VERDICTS.get(homework_status)
     if not verdict:
-        raise Exception(f'Статус отсутсвует или не задан: {verdict}')
+        raise Exception(f"Статус отсутсвует или не задан: {verdict}")
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -128,12 +134,14 @@ def main():
     check_tokens()
     bot = Bot(token=TELEGRAM_TOKEN)
     timestamp = 0
-    last_message = ''
+    last_message = ""
     while True:
         try:
-            response_api = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+            response_api = requests.get(
+                ENDPOINT, headers=HEADERS, params={"from_date": timestamp}
+            )
             response = get_api_answer(timestamp)
-            timestamp = response_api.json().get('current_date', timestamp)
+            timestamp = response_api.json().get("current_date", timestamp)
             homeworks = check_response(response)
             if len(homeworks) != 0:
                 homework = homeworks[0]
@@ -141,15 +149,15 @@ def main():
                 if last_message != message:
                     send_message(bot, message)
                     last_message = message
-                    logger.debug(f'Новый статус - {message}')
+                    logger.debug(f"Новый статус - {message}")
                 else:
-                    logger.debug('Отсутствуют новые статусы')
+                    logger.debug("Отсутствуют новые статусы")
             else:
-                logger.debug('Отсутствуют новые статусы')
-        except EmptyAnswerAPI:
-            logger.error(EmptyAnswerAPI)
+                logger.debug("Отсутствуют новые статусы")
+        except EmptyAnswerAPIError:
+            logger.error(EmptyAnswerAPIError)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f"Сбой в работе программы: {error}"
             if last_message != message:
                 send_message(bot, message)
                 last_message = message
@@ -158,9 +166,9 @@ def main():
             time.sleep(RETRY_PERIOD)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
     logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.DEBUG,
     )
